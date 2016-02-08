2fbe02322c8c078cb8d4b86e32a8fa22edee073e
柠烟夏季 <418826593@qq.com>
2016-02-08 14:11:03 +0800

    字典项下拉框标签

--- src/system/com/system/tags/SelectClauseTag.java
+++ src/system/com/system/tags/SelectClauseTag.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
@@ -12,30 +14,32 @@ import javax.servlet.jsp.tagext.TagSupport;
 import com.system.bean.Dict.DictClause;
 import com.system.service.IHibernateDao;
 import com.system.util.SpringUtils;
-
-public class ClauseNameTag extends TagSupport {
-	private static final long serialVersionUID = 7383074582043904436L;
-	private String dictCode;//字典编码
-	private String clauseCode;//字典项编码
-	private IHibernateDao<Object,String> hibernateDao;
+/**
+ * 根据字典项产生一个下拉框
+ * @author Administrator
+ *
+ */
+public class SelectClauseTag extends TagSupport {
+	private static final long serialVersionUID = -975708524751949380L;
+	private String name;
+	private String value;
+	private String dictCode;
 	private Map<String,String> dictMap;
-	public ClauseNameTag(){
+	
+	private IHibernateDao<Object,String> hibernateDao;
+	public SelectClauseTag(){
 		hibernateDao = SpringUtils.getBean("hibernateDao");
 	}
 	@SuppressWarnings("unchecked")
 	@Override
 	public int doStartTag() throws JspException {
-		//从pageConext当中获取这个字典的Map对象
 		dictMap = (Map<String, String>) pageContext.getAttribute(dictCode+"_clause_map");
 		if(dictMap == null){
-			//如果pageContext当中没有这个字典的信息,就查询这个字典的信息
 			List<DictClause> result = (List<DictClause>) hibernateDao.excuteQueryName("dictClauseList", dictCode);
 			dictMap = new LinkedHashMap<String,String>();
 			for(DictClause dictClause : result){
 				dictMap.put(dictClause.getClauseCode(), dictClause.getClauseName());
 			}
-			//并以LinkedHashMap对象的形式放入到pageContext当中去
-			//避免同一个页面多次执行查询
 			pageContext.setAttribute(dictCode+"_clause_map", dictMap);
 		}
 		return SKIP_BODY;
@@ -44,12 +48,32 @@ public class ClauseNameTag extends TagSupport {
 	public int doEndTag() throws JspException {
 		JspWriter out = pageContext.getOut();
 		try {
-			out.print(dictMap.get(clauseCode));
+			out.println("<select name=\""+(name==null?dictCode:name)+"\">");
+			Set<Entry<String,String>> entries = dictMap.entrySet();
+			for(Entry<String,String> entry : entries){
+				out.println("<option value=\"" + entry.getKey() + "\""
+							+ (entry.getKey().equals(value)?"selected":"")
+							+ ">"+entry.getValue() 
+							+ "</option>");
+			}
+			out.println("</select>");
 			out.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return EVAL_PAGE;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
 	}
 	public String getDictCode() {
 		return dictCode;
@@ -57,12 +81,4 @@ public class ClauseNameTag extends TagSupport {
 	public void setDictCode(String dictCode) {
 		this.dictCode = dictCode;
 	}
-	public String getClauseCode() {
-		return clauseCode;
-	}
-	public void setClauseCode(String clauseCode) {
-		this.clauseCode = clauseCode;
-	}
-	
-	
 }
