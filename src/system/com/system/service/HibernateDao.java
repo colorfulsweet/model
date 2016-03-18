c72738c422185bba3a3356cf26037ff02065d2f9
柠烟夏季 <418826593@qq.com>
2016-03-18 19:03:37 +0800

    Spring与Hibernate整合-事务管理

--- src/system/com/system/service/HibernateDao.java
+++ src/system/com/system/service/HibernateDao.java
@@ -4,16 +4,16 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Projections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import com.system.tags.Page;
 import com.system.util.DataCache;
-import com.system.util.HibernateUtils;
 import com.system.util.ReflectUtils;
 
 /**
@@ -22,75 +22,36 @@ import com.system.util.ReflectUtils;
  *
  */
 @Repository
-public class HibernateDao<T, PK extends Serializable> 
+public class HibernateDao<T, PK extends Serializable> extends HibernateDaoSupport
 			implements IHibernateDao<T, PK>{
-	
-	@Autowired(required=true)
-	private HibernateUtils hibernateUtils;
 	
 	@Autowired(required=true)
 	private DataCache dataCache;
 	
+	@Autowired
+	public void setSessionFactoryOverride(SessionFactory sessionFactory){
+		super.setSessionFactory(sessionFactory);
+	}
+	
 	@Override
 	public void save(T item) {
-		Session session = null;
-		try{
-			session = hibernateUtils.getSession();
-			session.save(item);
-			session.getTransaction().commit();
-			dataCache.cacheData(item);
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
-		}
-		
+		this.getSession().save(item);
+		dataCache.cacheData(item);
 	}
 	@Override
 	public void update(T item) {
-		Session session = null;
-		try{
-			session = hibernateUtils.getSession();
-			session.update(item);
-			session.getTransaction().commit();
-			dataCache.cacheData(item);
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
-		}
+		this.getSession().update(item);
+		dataCache.cacheData(item);
 	}
 	@Override
 	public void saveOrUpdate(T item) {
-		Session session = null;
-		try{
-			session = hibernateUtils.getSession();
-			session.saveOrUpdate(item);
-			session.getTransaction().commit();
-			dataCache.cacheData(item);
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
-		}
+		this.getSession().saveOrUpdate(item);
+		dataCache.cacheData(item);
 	}
 	@Override
 	public void del(T item) {
-		Session session = null;
-		try{
-			session = hibernateUtils.getSession();
-			session.delete(item);
-			session.getTransaction().commit();
-			dataCache.removeObject(item);
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
-		}
+		this.getSession().delete(item);
+		dataCache.removeObject(item);
 	}
 	@SuppressWarnings("unchecked")
 	@Override
@@ -99,64 +60,30 @@ public class HibernateDao<T, PK extends Serializable>
 			this.del(item);
 			return;
 		}
-		Session session = null;
-		try{
-			session = hibernateUtils.getSession();
-			item = (T) session.get(item.getClass(),
-					ReflectUtils.getItemField(item, "id").toString());
-			session.delete(item);
-			session.getTransaction().commit();
-			dataCache.removeObject(item);
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally{
-			hibernateUtils.closeSession(session);
-		}
+		Session session = this.getSession();
+		item = (T) session.get(item.getClass(),
+				ReflectUtils.getItemField(item, "id").toString());
+		session.delete(item);
+		dataCache.removeObject(item);
 	}
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<T> dir(Class<?> cls) {
-		Session session = null;
-		List<T> result = null;
-		try{
-			session = hibernateUtils.getSession();
-			result = session.createCriteria(cls).list();
-			session.getTransaction().commit();
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
-		}
+		List<T> result = this.getSession().createCriteria(cls).list();
 		return result;
 	}
 	@SuppressWarnings("unchecked")
 	@Override
 	public void dir(Class<?> cls,Page page){
-		Session session = null;
-		List<T> result = null;
-		int count = 0;
-		try{
-			session = hibernateUtils.getSession();
-			//聚合函数查询记录总数
-			Criteria criteria = session.createCriteria(cls)
-							.setProjection(Projections.rowCount());
-			count = (Integer) criteria.uniqueResult();
-			//查询当前页记录内容
-			criteria.setProjection(null);
-			result = criteria.setFirstResult(page.getRowStart())
-					.setMaxResults(page.getPageSize())
-					.list();
-			session.getTransaction().commit();
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
-		}
+		//聚合函数查询记录总数
+		Criteria criteria = this.getSession().createCriteria(cls)
+						.setProjection(Projections.rowCount());
+		int count = (Integer) criteria.uniqueResult();
+		//查询当前页记录内容
+		criteria.setProjection(null);
+		List<T> result = criteria.setFirstResult(page.getRowStart())
+				.setMaxResults(page.getPageSize())
+				.list();
 		page.setRowCount(count);
 		page.setResult(result);
 	}
@@ -169,107 +96,53 @@ public class HibernateDao<T, PK extends Serializable>
 			return item;
 		}
 		//如果缓存区当中未找到,则进行数据库查询
-		Session session = null;
-		try{
-			session = hibernateUtils.getSession();
-			item = (T) session.get(cls,id);
-			session.getTransaction().commit();
-			dataCache.cacheData(item);
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
-		}
+		item = (T) this.getSession().get(cls,id);
+		dataCache.cacheData(item);
 		return item;
 	}
 	@Override
 	public List<?> excuteQuery(String hql,Object... params){
-		Session session = null;
-		List<?> result = null;
-		try{
-			session = hibernateUtils.getSession();
-			Query query = session.createQuery(hql);
-			if(params != null){
-				for(int index=0 ; index<params.length ; index++){
-					query.setParameter(index, params[index]);
-				}
+		Query query = this.getSession().createQuery(hql);
+		if(params != null){
+			for(int index=0 ; index<params.length ; index++){
+				query.setParameter(index, params[index]);
 			}
-			result = query.list();
-			session.getTransaction().commit();
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
 		}
+		List<?> result = query.list();
 		return result;
 	}
 	@Override
 	public List<?> excuteSQLQuery(String sql,Object... params){
-		Session session = null;
-		List<?> result = null;
-		try{
-			session = hibernateUtils.getSession();
-			Query query = session.createSQLQuery(sql);
-			if(params != null){
-				for(int index=0 ; index<params.length ; index++){
-					query.setParameter(index, params[index]);
-				}
+		Query query = this.getSession().createSQLQuery(sql);
+		if(params != null){
+			for(int index=0 ; index<params.length ; index++){
+				query.setParameter(index, params[index]);
 			}
-			result = query.list();
-			session.getTransaction().commit();
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
 		}
+		List<?> result = query.list();
 		return result;
 	}
 	
 	@Override
 	public List<?> excuteQueryName(String queryName, Object... params) {
-		Session session = null;
-		List<?> result = null;
-		try{
-			session = hibernateUtils.getSession();
-			Query query = session.getNamedQuery(queryName);
-			if(params != null){
-				for(int index=0 ; index<params.length ; index++){
-					query.setParameter(index, params[index]);
-				}
+		Query query = this.getSession().getNamedQuery(queryName);
+		if(params != null){
+			for(int index=0 ; index<params.length ; index++){
+				query.setParameter(index, params[index]);
 			}
-			result = query.list();
-			session.getTransaction().commit();
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
 		}
+		List<?> result = query.list();
 		return result;
 	}
 	@Override
 	public int excuteUpdate(String hql, Object... params) {
-		Session session = null;
-		int lines = 0;
-		try{
-			session = hibernateUtils.getSession();
-			Query query = session.createQuery(hql);
-			if(params != null){
-				for(int index=0 ; index<params.length ; index++){
-					query.setParameter(index, params[index]);
-				}
+		Query query = this.getSession().createQuery(hql);
+		if(params != null){
+			for(int index=0 ; index<params.length ; index++){
+				query.setParameter(index, params[index]);
 			}
-			lines = query.executeUpdate();
-			session.getTransaction().commit();
-		} catch (HibernateException e){
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		} finally{
-			hibernateUtils.closeSession(session);
 		}
+		int lines = query.executeUpdate();
 		return lines;
 	}
 }
