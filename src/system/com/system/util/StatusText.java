2895d468a936a8b9b12ccde1da3b59d620fa4e11
柠烟夏季 <418826593@qq.com>
2016-03-27 18:43:10 +0800

    消息与代码分离处理

--- src/system/com/system/util/StatusText.java
+++
@@ -1,7 +0,0 @@
-package com.system.util;
-
-public interface StatusText {
-	public static String SUCCESS = "success";
-	public static String FAILED = "failed";
-	public static String ERROR = "error";
-}
